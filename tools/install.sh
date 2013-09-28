8fa50b885b5da7070cd0f9ccc47a9b18141938c5
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2013-09-28 23:12:06 +0200

    Add script that creates the right environment

---
+++ tools/install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# install dependencies
+sudo apt-get install libudev-dev python-serial
+
+# install node
+node_version=0.8.22 # 0.10 and 0.11 don't work
+node_folder=node-v${node_version}-linux-arm-pi
+node_url=http://nodejs.org/dist/v${node_version}/${node_folder}.tar.gz
+wget $node_url
+tar xvzf ${node_folder}.tar.gz
+rm -rf ${node_folder}.tar.gz
+mkdir ~/node
+mv ${node_folder} ~/node
+cd ~/node && ln -s ${node_folder} 0
+
+# install node-openvg-canvas (takes a while)
+sudo apt-get install libfreetype6 libfreetype6-dev libfreeimage3 libfreeimage-dev
+~/node/0/bin/npm install --global openvg-canvas
+
+# make sure we can load node modules
+echo "export NODE_PATH=$HOME/node/0/lib/node_modules:./node_modules:$NODE_PATH" >> ~/.profile
