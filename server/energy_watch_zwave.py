e032c2b0c61cdb32196cad55dd6f4961150ad45c
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2013-10-24 01:49:38 +0200

    Make changing colors work
    Fixes #19

--- server/energy_watch_zwave.py
+++ server/energy_watch_zwave.py
@@ -127,8 +127,7 @@ class Plug(object):
     return kWh
 
   def _set_option(self, register, value):
-    #http://sensorserver:8083/ZWaveAPI/Run/devices[4].instances[0].commandClasses[0x70].Set(61,3,1)    
-    command = 'devices[%d].instances[0].commandClasses[0x70].Set(%d,%d,1)' % \
+    command = 'devices[%d].instances[0].commandClasses[112].Set(%d,%d,1)' % \
         (self.id, register, value)
     print 'run:', command
     self.zway.run(command)
