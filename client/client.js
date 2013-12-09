fdcfa14b466689b7a073293a1c6b574b39ef6d45
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2013-12-09 12:16:55 +0100

    Make node client retry connecting on startup
    Fixes issue #22.

--- client/client.js
+++ client/client.js
@@ -3,9 +3,21 @@ var Canvas = require('openvg-canvas');
 var EventSource = require('eventsource');
 var request = require('request');
 
-request('http://localhost:8000/config', function(error, response, body) {
-  var config = JSON.parse(body);
-  var source = new EventSource('http://localhost:8000/data');
-  var canvas = new Canvas;
-  var app = new App(config, canvas, source);
-});
+var init = function() {
+  request({
+    url: 'http://localhost:8000/config',
+    timeout: 1000
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Could not contact the Python server. Trying again in 1s...');
+      setTimeout(init, 1000);
+    } else {
+      var config = JSON.parse(body);
+      var source = new EventSource('http://localhost:8000/data');
+      var canvas = new Canvas;
+      var app = new App(config, canvas, source);
+    }
+  });
+};
+
+init();
