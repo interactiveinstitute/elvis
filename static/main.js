8777abe6a5308ede5ca1ec17042a66396ab98cfe
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2014-01-26 22:27:36 +0100

    Add GPL 3 notice above source code files

--- static/main.js
+++ static/main.js
@@ -1,3 +1,21 @@
+/*
+ * This file is part of (e)lVis.
+ * Copyright 2013-2014 Interactive Institute Swedish ICT AB.
+ *
+ * (e)lVis is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * (e)lVis is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with (e)lVis.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 'use strict';
 
 window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function(callback) {
